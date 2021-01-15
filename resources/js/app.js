f7395b40813894c71d2298e5721c3aa012348b08
Tahina <to.tahina@gmail.com>
2021-01-15 10:16:42 +0300

    Implemented Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+
+// import App from './vue/app.vue'
+Vue.component('site-header', require('./components/SiteHeader.vue').default);
+Vue.component('left-sidebar', require('./components/LeftSidebar.vue').default);
+Vue.component('home-page', require('./components/app.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,127 @@
-@extends('layouts.app')
-@section('content')
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Tableau de bord</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Accueil</a></li>
-              <li class="breadcrumb-item active">Tableau de bord</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="card-body table-responsive p-0">
-          <div class="alert alert-success alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <h5><i class="icon fas fa-check"></i> Message</h5>
-            Authentification réussie.
-          </div>
-        </div><!--  .card-body -->
-      </div><!-- /.container-fluid -->
+  <title>{{ config('app.name') }}</title>
+
+  <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Technicut') }}</title>
+
+    <!-- Scripts -->
+    {{-- <script src="{{ url('/') }}/js/app.js" defer></script> --}}
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    {{-- <link href="{{ url('/') }}/public/css/app.css" rel="stylesheet"> --}}
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{ url('/') }}/admin-lte/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ url('/') }}/admin-lte/dist/css/adminlte.min.css">
+  <!-- Include Bootstrap Datepicker -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" />
+  
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div id="app" class="wrapper">
+    <site-header></site-header>
+    <left-sidebar></left-sidebar>
+    
+
+    <!-- Content Wrapper. Contains page content -->
+    <home-page></home-page>
+    <!-- /.content-wrapper -->
+
+  </div> <!-- #app -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+        <h5>Menu</h5>
+        Sidebar content
     </div>
-    <!-- /.content -->
-@endsection
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      v 0.1
+    </div>
+    <!-- Default to the left -->
+    Copyright &copy; 2020 
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<script src="{{ url('/') }}/js/app.js"></script>
+<!-- jQuery -->
+<script src="{{ url('/') }}/admin-lte/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ url('/') }}/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- date picker -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{ url('/') }}/admin-lte/dist/js/adminlte.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function () {
+    $("#startdate_datepicker").datepicker({
+      format: "yyyy-mm-dd"
+    });
+    $("#enddate_datepicker").datepicker({
+      format: "yyyy-mm-dd"
+    });
+
+    // onclick display
+    $('#display-logs').click(function(e) {
+      var startDate = $("#startdate_datepicker").val();
+      var endDate = $("#enddate_datepicker").val();
+      var baseUrl = '{{ url('/') }}/calls/list/' + startDate + '/' + endDate;
+      //alert(startDate);
+      window.location.href = baseUrl;
+      e.preventDefault(); // prevents default
+      return false; // al
+    });
+
+    // onclick import
+    $('#import-logs').click(function(e) {
+      var startDate = $("#startdate_datepicker").val();
+      var endDate = $("#enddate_datepicker").val();
+      var baseUrl = '{{ url('/') }}/calls/downloadlogs/' + startDate + '/' + endDate;
+      //alert(startDate);
+      window.location.href = baseUrl;
+      e.preventDefault(); // prevents default
+      return false; // al
+    });
+
+    // onclick export
+    $('#export-logs').click(function(e) {
+      var startDate = $("#startdate_datepicker").val();
+      var endDate = $("#enddate_datepicker").val();
+      var baseUrl = '{{ url('/') }}/calls/export/' + startDate + '/' + endDate;
+      //alert(startDate);
+      window.location.href = baseUrl;
+      e.preventDefault(); // prevents default
+      return false; // al
+    });
+  });
+</script>
+</body>
+</html>
